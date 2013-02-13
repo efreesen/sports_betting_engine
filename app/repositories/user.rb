6d4715709ddc21be2e19a9769d26904278f05822
Caio Torres <efreesen@gmail.com>
2013-02-13 17:01:53 -0200

    Added bets support and a README file

--- app/repositories/user.rb
+++
@@ -1,7 +0,0 @@
-require "active_repository"
-
-class User < ActiveRepository::Base
-  fields :name, :email, :external_id
-
-  validates_uniqueness_of :external_id
-end
--- spec/spec_helper.rb
+++
@@ -1,3 +0,0 @@
-require 'rspec'
-require 'rspec/autorun'
-require 'shoulda/matchers/active_model'
